5ce87d7c3e4ad54b5af939e4b0703326d68516fb
musa <musa@macos.local>
2016-12-05 10:57:28 +0800

    first commit

---
+++ MessagesExtension/MessagesViewController.h
@@ -0,0 +1,13 @@
+//
+//  MessagesViewController.h
+//  MessagesExtension
+//
+//  Created by musa on 12/5/16.
+//  Copyright © 2016 zhouming. All rights reserved.
+//
+
+#import <Messages/Messages.h>
+
+@interface MessagesViewController : MSMessagesAppViewController
+
+@end
